0ef58ad00d124fe8ff76bcd4451fb2e574724be4
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-09-30 10:52:29 -0400

    Prepping for React Router integration
    * moved `db` to `src/db/index.ts`

---
+++ src/db/index.ts
@@ -0,0 +1,45 @@
+import 'dotenv/config'
+import { drizzle } from 'drizzle-orm/postgres-js';
+
+// FOR TESTING DB CONNECTION
+//
+// import { eq } from 'drizzle-orm';
+// import { users } from './schema'
+
+export const db = drizzle(process.env.DATABASE_URL!);
+
+
+// FOR TESTING DB CONNECTION
+//
+// async function main() {
+//   const user: typeof users.$inferInsert = {
+//     name: 'John',
+//     email: 'john123@test.com'
+//   };
+
+//   await db.insert(users).values(user);
+//   console.log('New user created!')
+
+//   const allUsers = await db.select().from(users);
+//   console.log('Getting all users from the database:', allUsers)
+//   /*
+//   const users: {
+//     id: number;
+//     name: string;
+//     email: string;
+//   }[]
+//   */
+
+//   await db
+//     .update(users)
+//     .set({ name: 'Steve', })
+//     .where(eq(users.email, user.email))
+//   console.log('User info updated!')
+
+//   await db
+//     .delete(users)
+//     .where(eq(users.email, user.email))
+//   console.log('User deleted!')
+//   }
+
+// main();
